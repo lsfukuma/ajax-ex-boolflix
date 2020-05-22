f723e0f6cbb2de4abb0fbf1c7cb56dd4aef08702
Leticia <lsf@protonmail.ch>
2020-05-22 14:47:37 +0200

    commit1

---
+++ main.js
@@ -0,0 +1,10 @@
+// ciao ragazzi, oggi diamo il via ad un nuovo esercizio lungo: Boolflix! :visage_avec_yeux_en_étoiles:
+// Oggi vi chiedo di concentrarvi sulla prima milestone, poi lunedì scopriamo le altre :laptop_parrot:Per prima cosa, come abbiamo visto a lezione, bisogna registrarsi a tmdb e richiedere la propria API KEY, necessaria per poter inviare richieste ajax.Predisporre quindi un layout molto semplice con una barra di ricerca e un pulsante: al click sul pulsante fare partire una chiamata ajax a tmdb per recuperare i film che corrispondo alla query di ricerca inserita dall'utente.
+// Ciclare i risultati e per ogni film restituito, stamparne in pagina:
+//
+//     titolo
+//     titolo originale
+//     lingua
+//     voto
+//
+// Come vi dicevo, per il momento non è importante l'aspetto grafico: i risultati possono essere inseriti in pagina come semplici ul, anche senza handlebars.
