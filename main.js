4042c377992fb43830e68882c9a23eaeab9acbe3
Leticia <lsf@protonmail.ch>
2020-05-22 15:43:14 +0200

    chiamata ajax a tmdb

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
-// ciao ragazzi, oggi diamo il via ad un nuovo esercizio lungo: Boolflix! :visage_avec_yeux_en_étoiles:
-// Oggi vi chiedo di concentrarvi sulla prima milestone, poi lunedì scopriamo le altre :laptop_parrot:Per prima cosa, come abbiamo visto a lezione, bisogna registrarsi a tmdb e richiedere la propria API KEY, necessaria per poter inviare richieste ajax.Predisporre quindi un layout molto semplice con una barra di ricerca e un pulsante: al click sul pulsante fare partire una chiamata ajax a tmdb per recuperare i film che corrispondo alla query di ricerca inserita dall'utente.
+// Predisporre quindi un layout molto semplice con una barra di ricerca e un pulsante: al click sul pulsante fare partire una chiamata ajax a tmdb per recuperare i film che corrispondo alla query di ricerca inserita dall'utente.
 // Ciclare i risultati e per ogni film restituito, stamparne in pagina:
 //
 //     titolo
 //     titolo originale
 //     lingua
 //     voto
-//
-// Come vi dicevo, per il momento non è importante l'aspetto grafico: i risultati possono essere inseriti in pagina come semplici ul, anche senza handlebars.
+
+//quando clicco sul pulsante search
+$('#btn-search').click(function(){
+    var search = $('#input-search').val()
+    console.log(search);
+    //fare partire una chiamata ajax a tmdb
+    $.ajax({
+        'url': 'https://api.themoviedb.org/3/search/movie',
+        'method': 'GET',
+        'data': {
+            'api_key':'239041d19fa5a16a25dff0efb29a6dec',
+            'query': search ,
+        },
+        'success': function(){
+            
+        },
+        'error': function(){
+
+        }
+
+    }) //ajax
+
+
+
+
+}); // click sul pulsante
