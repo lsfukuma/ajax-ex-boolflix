1a5281eeb529cb285afbfd2ca9b177c41dedd2f7
Leticia <lsf@protonmail.ch>
2020-05-25 13:11:01 +0200

    ajax ricerca series

--- main.js
+++ main.js
@@ -1,20 +1,14 @@
-// Predisporre quindi un layout molto semplice con una barra di ricerca e un pulsante: al click sul pulsante fare partire una chiamata ajax a tmdb per recuperare i film che corrispondo alla query di ricerca inserita dall'utente.
-// Ciclare i risultati e per ogni film restituito, stamparne in pagina:
-//
-//     titolo
-//     titolo originale
-//     lingua
-//     voto
-
 //quando clicco sul pulsante search
-$('#btn-search').click(function(){
-    searchMovie();
+$('.btn-search').click(function(){
+    // searchMovie();
+    searchSerie();
 }); // click sul pulsante
 
 //ricerca premendo tasto enter
-$('#input-search').keyup(function(event){
+$('.input-search').keyup(function(event){
     if (event.which == 13) {
         searchMovie();
+        searchSerie();
     }
 })
 
@@ -22,17 +16,64 @@ $('#input-search').keyup(function(event){
 var source   = $("#results-template").html();
 var template = Handlebars.compile(source)
 
+// Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
+function searchSerie() {
+    var search = $('.input-search').val().trim()
+    console.log(search);
+    resetInput();
+    if (search.length >1){
+        $.ajax({
+            'url': 'https://api.themoviedb.org/3/search/tv',
+            'method': 'GET',
+            'data': {
+                'api_key':'239041d19fa5a16a25dff0efb29a6dec',
+                'query': search ,
+                'language': 'pt',
+            },
+            'success': function(serie){
+                var series = serie.results
+                console.log(series);
+                //con il ciclo for recupero gli oggetti dentro l'array
+                for (var i = 0; i < series.length; i++) {
+                    var seriesResults = series[i]
+                    console.log(seriesResults);
+                    //dall'OGGETTO prendo le informazioni che mi servono
+                    var nameSerie = seriesResults.name;
+                    var originalNameSerie = seriesResults.original_name;
+                    var originalLg = seriesResults.original_language;
+                    var averageVote = seriesResults.vote_average;
+                    console.log(nameSerie);
+                    console.log(originalNameSerie);
+                    console.log(originalLg);
+                    console.log(averageVote);
+                    var context = {
+                        'title': nameSerie,
+                        'title-original': originalNameSerie,
+                        'original-language': originalLg ,
+                        'average-vote': averageVote
+                    };
+                    var html = template(context);
+                    //appendo con Handlebars
+                    $('.container').append(html)
+
+                } //ciclo for
+            },
+            'error': function(){
+                alert('error')
+            }
+        }) //ajax
+    } else {
+        alert('prob serie')
+    }// fine if
+}// fine funzione ricerca serie
+
 //funzione per trovare il film
 function searchMovie() {
-    var search = $('#input-search').val().trim()
+    var search = $('.input-search').val().trim()
     console.log(search);
+    resetInput(); //richiamo la funzione per resettare l'input
     //controllo che l'utente abbia digitato qualcosa
     if (search.length > 1) {
-        //svuoto l'input
-        $('#input-search').val('');
-        //svuoto il html per la prossima ricerca
-        $('.card').empty();
-        //faccio partire una chiamata ajax a tmdb
         $.ajax({
             'url': 'https://api.themoviedb.org/3/search/movie',
             'method': 'GET',
@@ -66,7 +107,7 @@ function searchMovie() {
                     var html    = template(context);
                     //appendo con Handlebars
                     $('.container').append(html)
-                    
+
                 } //ciclo for
             },
             'error': function(){
@@ -76,4 +117,12 @@ function searchMovie() {
     } else {
         alert('digita qualcosa')
     }// fine if
-}; // fine funzione
+}; // fine funzione ricerca film
+
+function resetInput(){
+    //svuoto l'input
+    $('.input-search').val('');
+    //svuoto il html per la prossima ricerca
+    $('.card').empty();
+    //faccio partire una chiamata ajax a tmdb
+}
