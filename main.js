172fa764689398beefb87a93a5aa037292b65c78
Leticia <lsf@protonmail.ch>
2020-05-26 12:47:22 +0200

    aggiunte altre banderine, codice poco allegerito, inizio mil 3

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
+//
+var ajaxUrl = 'https://api.themoviedb.org/3/'
+var apiKey = '239041d19fa5a16a25dff0efb29a6dec'
+var posterUrl = 'https://image.tmdb.org/t/p/w300 '
 //ricerca cliccando sul pulsante search
 $('.btn-search').click(function(){
-    searchMovie();
-    // searchSerie();
+    search();
 });
 
 //ricerca premendo tasto enter
 $('.input-search').keyup(function(event){
     if (event.which == 13) {
-        searchMovie();
-        // searchSerie();
+        search();
     }
 })
 
@@ -16,73 +18,23 @@ $('.input-search').keyup(function(event){
 var source   = $("#results-template").html();
 var template = Handlebars.compile(source)
 
-function searchSerie() {
-    var searchSerie = $('.input-search').val().trim();
-    console.log(searchSerie);
-    resetInput();
-    if (searchSerie != ''){
-        $.ajax({
-            'url': 'https://api.themoviedb.org/3/search/tv',
-            'method': 'GET',
-            'data': {
-                'api_key':'239041d19fa5a16a25dff0efb29a6dec',
-                'query': searchSerie ,
-            },
-            'success': function(serie){
-                var series = serie.results
-                console.log(series);
-                //con il ciclo for recupero gli oggetti dentro l'array
-                for (var i = 0; i < series.length; i++) {
-                    var seriesResults = series[i]
-                    console.log(seriesResults);
-                    //dall'OGGETTO prendo le informazioni che mi servono
-                    var nameSerie = seriesResults.name;
-                    var originalNameSerie = seriesResults.original_name;
-                    var originalLg = seriesResults.original_language;
-                    var averageVote = seriesResults.vote_average;
-                    console.log(nameSerie);
-                    console.log(originalNameSerie);
-                    console.log(originalLg);
-                    console.log(averageVote);
-                    var context = {
-                        'title': nameSerie,
-                        'title-original': originalNameSerie,
-                        'original-language': originalLg ,
-                        'average-vote': averageVote
-                    };
-                    var html = template(context);
-                    //appendo con Handlebars
-                    $('.container').append(html)
-                } //ciclo for
-            },
-            'error': function(){
-                alert('error')
-            }
-        }) //ajax
-    } else {
-        alert('prob parte serie')
-    }// fine if
-}// fine funzione ricerca serie
-
-//funzione per trovare il film
-function searchMovie() {
+//funzione per trovare il film e anche le serie
+function search() {
     var search = $('.input-search').val().trim()
-    console.log(search);
     resetInput(); //richiamo la funzione per resettare l'input
     //controllo che l'utente abbia digitato qualcosa
     if (search.length > 0) {
         //faccio partire una chiamata ajax a tmdb
         $.ajax({
-            'url': 'https://api.themoviedb.org/3/search/movie',
+            'url': ajaxUrl + 'search/movie',
             'method': 'GET',
             'data': {
-                'api_key':'239041d19fa5a16a25dff0efb29a6dec',
+                'api_key': apiKey ,
                 'query': search ,
                 'language': 'pt',
             },
             'success': function(data){
                 var movies = data.results
-                console.log(movies);
                 //con il ciclo for recupero gli oggetti dentro l'array
                 for (var i = 0; i < movies.length; i++) {
                     var moviesResults = movies[i]
@@ -92,16 +44,14 @@ function searchMovie() {
                     var originalName = moviesResults.original_title;
                     var originalLg = moviesResults.original_language;
                     var averageVote = moviesResults.vote_average;
-                    // console.log(nameMovie);
-                    // console.log(originalName);
-                    // console.log(originalLg);
-                    // console.log(averageVote);
+                    var posterMovie = moviesResults.poster_path
                     //Handlebars
                     var context = {
+                        'poster': posterUrl + posterMovie,
                         'title': nameMovie,
                         'title-original': originalName,
                         'original-language': flags(originalLg),
-                        'average-vote': averageVote + rating(averageVote)
+                        'average-vote': rating(averageVote)
                     };
                     //append handlebars
                     var html    = template(context);
@@ -111,11 +61,46 @@ function searchMovie() {
             'error': function(){
                 alert('error')
             }
-        }) //ajax
+        }) //ajax film
+        $.ajax({
+            'url': ajaxUrl + 'search/tv',
+            'method': 'GET',
+            'data': {
+                'api_key': apiKey ,
+                'query': search ,
+                'language': 'pt' ,
+            },
+            'success': function(serie){
+                var series = serie.results
+                //con il ciclo for recupero gli oggetti dentro l'array
+                for (var i = 0; i < series.length; i++) {
+                    var seriesResults = series[i]
+                    //dall'OGGETTO prendo le informazioni che mi servono
+                    var nameSerie = seriesResults.name;
+                    var originalNameSerie = seriesResults.original_name;
+                    var originalLg = seriesResults.original_language;
+                    var averageVote = seriesResults.vote_average;
+                    var posterSerie = seriesResults.poster_path
+                    var context = {
+                        'poster': posterUrl + 'posterSerie' ,
+                        'title': nameSerie,
+                        'title-original': originalNameSerie,
+                        'original-language': flags(originalLg) ,
+                        'average-vote': rating(averageVote)
+                    };
+                    var html = template(context);
+                    //appendo con Handlebars
+                    $('.container').append(html)
+                } //ciclo for
+            },
+            'error': function(){
+                alert('error')
+            }
+        }) //ajax series
     } else {
         alert('digita qualcosa')
-    }// fine if
-}; // fine funzione ricerca film
+    }// fine if che controlla l'input
+}; // fine funzione ricerca film/serie
 
 function resetInput(){
     //svuoto l'input
@@ -141,7 +126,7 @@ function rating(vote){
 
 //funzione per aggiungere le bandiere disponibili
 function flags(language){
-    var flagsLanguage = ['en', 'fr' , 'it']
+    var flagsLanguage = ['en', 'fr' , 'it' , 'ru' , 'pt' , 'de' , 'es' , 'da' , 'ja']
     if(flagsLanguage.includes(language)){
         return '<img src="img/' + language + '.svg" alt="">';
     } else {
